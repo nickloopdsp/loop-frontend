1a02b2b93b441ff8433bff777498ed0853b25cba
mohsankhan5575 <mohsan.khan5575@gmail.com>
2025-06-20 22:43:25 +0500

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,16 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "@/providers/ThemeProvider";
-import { ChatProvider } from "@/contexts/ChatContext";
-import { ArtistProvider } from "@/contexts/ArtistContext";
-import { ModeProvider } from "@/contexts/ModeContext";
 import { AppRoutes } from "./routes/AppRoutes";
-
-
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="loop-ui-theme">
       <QueryClientProvider client={queryClient}>
-        <ArtistProvider>
-          <ModeProvider>
-            <ChatProvider>
-              <TooltipProvider>
-                <Toaster />
-                <AppRoutes />
-              </TooltipProvider>
-            </ChatProvider>
-          </ModeProvider>
-        </ArtistProvider>
+        <TooltipProvider>
+          <Toaster />
+          <AppRoutes />
+        </TooltipProvider>
       </QueryClientProvider>
     </ThemeProvider>
   );
